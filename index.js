2cbf0a5c5dc4f66597b1c358b2182d08d17c6755
nyeoro-bong <68091561+nyeoro-bong@users.noreply.github.com>
2021-10-03 12:14:46 +0900

    DELETEリクエスト表示の実装

--- index.js
+++ index.js
@@ -2,13 +2,35 @@
 const http = require('http');
 const server = http
   .createServer((req, res) => {
+    const now = new Date();
     console.info(
-      '[' + new Date() + '] Requested by ' + req.socket.remoteAddress
+      '[' + now + '] Requested by ' + req.socket.remoteAddress
     );
     res.writeHead(200, {
       'Content-Type': 'text/plain; charset=utf-8'
     });
-    res.write(req.headers['user-agent']);
+
+    switch (req.method) {
+      case 'GET':
+        res.write('GET' + req.url);
+        break;
+      case 'POST':
+        res.write('POST' + req.url);
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData = rawData + chunk;
+          })
+          .on('end', () => {
+            console.info('[' + now + '] Data posted: ' + rawData);
+          });
+        break;
+      case 'DELETE':
+        res.write('DELETE' + req.url);
+        break;
+      default:
+        break;
+    }
     res.end();
   })
   .on('error', e => {
