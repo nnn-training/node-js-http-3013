af67d2e0000c74b3c8b88f012c6c74102881d243
largekind <3191524-largekind@users.noreply.gitlab.com>
2022-12-19 22:43:16 +0900

    Add HTTP method function

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 const http = require('http');
 const server = http
   .createServer((req, res) => {
-    console.info(
-      `[${new Date()}] Requested by ${req.socket.remoteAddress}`
-    );
+     const now = new Date();
+    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       'Content-Type': 'text/plain; charset=utf-8'
     });
-    res.write(req.headers['user-agent']);
+    //各HTTPメソッドごとに分岐
+    switch (req.method) {
+      case 'GET':
+        res.write(`GET ${req.url}`);
+        break;
+      case 'POST':
+        res.write(`POST ${req.url}`);
+        let rawData = '';
+        req
+          .on('data', chunk => {
+            rawData += chunk;
+          })
+          .on('end', () => {
+            console.info(`[${now}] Data posted: ${rawData}`);
+          });
+        break;
+      default:
+        break;
+    }
     res.end();
   })
   .on('error', e => {
